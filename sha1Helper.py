a3c7a4a42682593a0be10acfadd8400a8d795547
iambirdpeng <iambirdpeng@163.com>
2018-08-26 19:18:05 +0800

    增加hashlib--sha1--使用步骤注释，更改转变函数名为trans_sha1_pwd

--- sha1Helper.py
+++ sha1Helper.py
@@ -1,11 +1,14 @@
 from hashlib import sha1
+# 第一步创建sha1对象，
+# 第二步用对象.update加密pwd ,注意用encode转码，
+# 第三部用对象.hexdigest()拿到加密后的码
 
 
-def print_pwd(orignal_pwd):
+def trans_sha1_pwd(orignal_pwd):
     s1 = sha1()
-    s1.update(orignal_pwd.encode('utf-8'))
+    s1.update(orignal_pwd.encode('utf-8'))     #注意要用encode转码
     return s1.hexdigest()
 
 if __name__ == '__main__':
     orignal_pwd = input("请输入要加密的密码：")
-    print(print_pwd(orignal_pwd.encode('utf-8')))
+    print(trans_sha1_pwd(orignal_pwd))
